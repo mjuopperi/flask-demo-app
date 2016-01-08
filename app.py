41b631243a7201f86a155c125f9f722ea5e205e3
Joonas Jauhiainen <joonas.jauhiainen@kone.com>
2016-01-08 15:58:18 +0200

    Added leffatykki integration.

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
 from flask import Flask, render_template
 from services.finnkino import FinnKinoXML
+from services.leffatykki import LeffaTykkiRSS
 app = Flask(__name__)
 fk = FinnKinoXML()
+lt = LeffaTykkiRSS()
+
+def get_movies_with_reviews(area_code):
+	movie_container = {}
+	movies = fk.get_movies_from_area(area_code)
+	reviews = lt.get_movie_reviews()
+	for id, movie in movies.iteritems():
+		review_link = ""
+		title = movie['title']
+		if title in reviews:
+			review_link = reviews[movie['title']]
+		movie_container[id] = {
+			'title': movie['title'],
+			'rating': movie['rating'],
+			'genres': "".join(movie['genres']),
+			'review': review_link
+		}
+	return movie_container
+
 @app.route('/')
 def index():
 	#return 'Hello World!'
 	areas = fk.get_area_codes()
+	movies = get_movies_with_reviews(1033)
 	data = {
-		'areas': areas
+		'areas': areas,
+		'movies': movies
 	}
 	return render_template('index.html', data=data)
 
--- services/finnkino.py
+++ services/finnkino.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 class FinnKinoXML(object):
     areas = {}
     area_url = "http://www.finnkino.fi/xml/TheatreAreas"
+    schedule_url = "http://www.finnkino.fi/xml/Schedule/"
     headers = {
         'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:43.0) Gecko/20100101 Firefox/43.0",
         'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
@@ -24,5 +25,25 @@ class FinnKinoXML(object):
 			name = area.find('Name').text
 			self.areas[id] = name
 
+    def get_movies_from_area(self, area_code):
+    	url = "{}?area={}".format(self.schedule_url, area_code)
+    	r = requests.get(url, headers=self.headers)
+    	root = ET.fromstring(r.content)
+    	shows = root.find('Shows').findall('Show')
+    	return self._parse_shows(shows)
+
+    def _parse_shows(self, shows):
+    	movies = {}
+    	for show in shows:
+    		event_id = show.find('EventID').text
+    		title = show.find('Title').text
+    		rating = show.find('RatingImageUrl').text
+    		genres = show.find('Genres').text.split(",")
+    		movies[event_id] = {
+    		    "title": title,
+    		    "rating": rating,
+    		    "genres": genres
+    		}
+    	return movies
 
 			
